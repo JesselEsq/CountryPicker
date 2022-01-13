b7d12e85515ea25077fb13c56e82a16906de5382
Jessel Esquinas <jessel.esquinas@cheetahdigital.com>
2022-01-13 13:31:07 +0800

    Add refresh control

--- CountryPicker/ViewControllers/CountriesViewController.swift
+++ CountryPicker/ViewControllers/CountriesViewController.swift
@@ -7,20 +7,25 @@
 
 import UIKit
 
-class CountriesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class CountriesViewController: UIViewController {
 
     @IBOutlet weak var countriesTableView: UITableView!
+    
+    private let refreshControl = UIRefreshControl()
     private var countries = [Country]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         fetchCountries()
         setupTableView()
+        setupRefreshControl()
     }
     
+    @objc
     private func fetchCountries() {
         CountryAPI.getCountries(completionHandler: { [weak self] countries in
             DispatchQueue.main.async {
+                self?.refreshControl.endRefreshing()
                 self?.countries = countries
                 self?.countriesTableView.reloadData()
             }
@@ -33,8 +38,32 @@ class CountriesViewController: UIViewController, UITableViewDelegate, UITableVie
         countriesTableView.register(CountryCell.nib(), forCellReuseIdentifier: CountryCell.nibName())
         countriesTableView.estimatedRowHeight = 80
         countriesTableView.rowHeight = 80
+        
     }
     
+    private func setupRefreshControl() {
+        refreshControl.addTarget(self, action: #selector(fetchCountries), for: .valueChanged)
+        countriesTableView.refreshControl = refreshControl
+    }
+
+}
+
+// MARK: - UITableViewDelegate
+extension CountriesViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let country = countries[indexPath.row]
+        let countryDetail = CountryDetailViewController()
+        countryDetail.country = country
+        let navigationViewController = UINavigationController(rootViewController: countryDetail)
+        navigationViewController.navigationBar.isHidden = false
+        navigationController?.pushViewController(countryDetail, animated: true)
+    }
+    
+}
+
+// MARK: - UITableViewDataSource
+extension CountriesViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return countries.count
@@ -51,14 +80,5 @@ class CountriesViewController: UIViewController, UITableViewDelegate, UITableVie
         return cell
     }
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let country = countries[indexPath.row]
-        let countryDetail = CountryDetailViewController()
-        countryDetail.country = country
-        let navigationViewController = UINavigationController(rootViewController: countryDetail)
-        navigationViewController.navigationBar.isHidden = false
-        navigationController?.pushViewController(countryDetail, animated: true)
-    }
-
 }
 
